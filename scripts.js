c5db341f334eae0c11343876be9eaa31613d6ca8
JoaoVSouto <joaovitorss99@hotmail.com>
2021-04-19 23:34:49 -0300

    Switch icon based on theme

--- scripts.js
+++ scripts.js
@@ -8,16 +8,36 @@
   const DARK_MODE_LOCAL_STORAGE_KEY = 'pokedex:darkModeOn';
 
   const darkModeToggler = document.getElementById('darkmode-toggler');
+  const darkModeTogglerIcon = darkModeToggler.querySelector('i');
+
+  const setSunIcon = () => {
+    darkModeTogglerIcon.className = '';
+    darkModeTogglerIcon.classList.add('fas', 'fa-sun');
+  };
+
+  const setMoonIcon = () => {
+    darkModeTogglerIcon.className = '';
+    darkModeTogglerIcon.classList.add('far', 'fa-moon');
+  };
 
   darkModeToggler.addEventListener('click', () => {
     halfmoon.toggleDarkMode();
 
     localStorage.setItem(DARK_MODE_LOCAL_STORAGE_KEY, halfmoon.darkModeOn);
+
+    if (halfmoon.darkModeOn) {
+      setMoonIcon();
+    } else {
+      setSunIcon();
+    }
   });
 
   const darkModeSelection = localStorage.getItem(DARK_MODE_LOCAL_STORAGE_KEY);
 
-  if (darkModeSelection === 'true') {
+  if (darkModeSelection === 'true' || darkModeSelection === null) {
     halfmoon.toggleDarkMode();
+    setMoonIcon();
+  } else {
+    setSunIcon();
   }
 })();
